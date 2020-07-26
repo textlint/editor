0ed4a4aae6ded8f053070b9ff2f74adc64aa98ce
azu <azuciao@gmail.com>
2020-07-26 18:19:12 +0900

    fix: add multiple request handler

--- packages/@textlint/compiler/src/CodeGenerator/worker-codegen.ts
+++ packages/@textlint/compiler/src/CodeGenerator/worker-codegen.ts
@@ -86,6 +86,17 @@ self.addEventListener('message', (event) => {
 const dictionaryStorage = openStorage("kuromoji");
 const BrowserDictionaryLoader = require("kuromoji/src/loader/BrowserDictionaryLoader");
 const zlib = require("zlibjs/bin/gunzip.min.js");
+class Deferred {
+    constructor() {
+        this.promise = new Promise((resolve, reject) => {
+            this.resolve = resolve;
+            this.reject = reject;
+        });
+    }
+}
+// InMemory Cache
+const dictionaryDeferred = new Deferred();
+const urlMap = new Map();
 BrowserDictionaryLoader.prototype.loadArrayBuffer = async function (url, callback) {
     // https://github.com/takuyaa/kuromoji.js/issues/37
     const fixedURL = url.replace("https:/", "https://");
@@ -94,6 +105,16 @@ BrowserDictionaryLoader.prototype.loadArrayBuffer = async function (url, callbac
         // console.log("return cache", cachedDictBuffer);
         return callback(null, cachedDictBuffer);
     }
+    // Suppress multiple request to same url at same time
+    if (urlMap.has(fixedURL)) {
+        return urlMap.get(fixedURL).promise.then(result => {
+            callback(null, result);
+        }).catch(error => {
+            callback(error);
+        });
+    }
+    const deferred = new Deferred();
+    urlMap.set(fixedURL, deferred);
     fetch(fixedURL).then(function (response) {
         if (!response.ok){
             return callback(response.statusText, null);
@@ -103,10 +124,12 @@ BrowserDictionaryLoader.prototype.loadArrayBuffer = async function (url, callbac
             var typed_array = gz.decompress();
             return dictionaryStorage.set(fixedURL, typed_array.buffer).then(() => {
                 // console.log("cached", fixedURL);
+                deferred.resolve(typed_array.buffer);
                 callback(null, typed_array.buffer);
             });
         });
     }).catch(function (exception) {
+        deferred.reject(exception);
         callback(exception, null);
     });
 };
