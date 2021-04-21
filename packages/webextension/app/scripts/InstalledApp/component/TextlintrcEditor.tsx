339cf6f9a3e8e7037840506d44631d57e4dfcca1
azu <azuciao@gmail.com>
2021-04-21 22:03:31 +0900

    feat(webextension): support to update Script
    fix https://github.com/textlint/editor/issues/32

--- packages/webextension/app/scripts/InstalledApp/component/TextlintrcEditor.tsx
+++ packages/webextension/app/scripts/InstalledApp/component/TextlintrcEditor.tsx
@@ -1,29 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { UnControlled as CodeMirror } from "react-codemirror2";
-import { ActionButton, Flex } from "@adobe/react-spectrum";
-import type { Editor } from "codemirror";
+import { Flex } from "@adobe/react-spectrum";
 
 require("codemirror/lib/codemirror.css");
 require("codemirror/mode/javascript/javascript");
 export type TextlintrcEditorProps = {
     textlintrc: string;
-    onSave: (textlintrc: string) => void;
+    onChange: (textlintrc: string) => void;
 };
 export const TextlintrcEditor = (props: TextlintrcEditorProps) => {
-    const [codeMirror, setCodemirror] = useState<Editor | null>(null);
-    const onSave = React.useCallback(() => {
-        if (!codeMirror) {
-            return;
-        }
-        const value = codeMirror.getValue();
-        try {
-            JSON.parse(value);
-        } catch (error) {
-            alert(".textlintrc should be JSON format. Parse Error:" + error);
-            return;
-        }
-        props.onSave(value);
-    }, [codeMirror]);
     return (
         <div className={"TextlintrcEditor"}>
             <Flex direction="column" gap="size-100">
@@ -37,11 +22,10 @@ export const TextlintrcEditor = (props: TextlintrcEditorProps) => {
                         },
                         lineNumbers: true
                     }}
-                    editorDidMount={(editor) => {
-                        setCodemirror(editor);
+                    onChange={(editor) => {
+                        props.onChange(editor.getValue());
                     }}
                 />
-                <ActionButton onPress={onSave}>Save</ActionButton>
             </Flex>
         </div>
     );
--- packages/webextension/app/scripts/InstalledApp/pages/EditPage.tsx
+++ packages/webextension/app/scripts/InstalledApp/pages/EditPage.tsx
@@ -1,36 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { TextlintrcEditor } from "../component/TextlintrcEditor";
 import { RouteComponentProps } from "@reach/router";
 import { usePort } from "../StateContext";
-import { ScriptMeta } from "../component/ScriptMeta";
 import "./EditPage.css";
-import { Flex } from "@adobe/react-spectrum";
+import { ActionButton, Flex, Form, TextField } from "@adobe/react-spectrum";
 import { logger } from "../../utils/logger";
 
 export type EditPageProps = {
     name?: string;
     namespace?: string;
 } & RouteComponentProps;
-export const EditPage = (props: EditPageProps) => {
-    const [script, setScript] = useState("");
-    const [meta, setMeta] = useState({
-        name: "",
-        namespace: "",
-        pattern: "",
-        ext: ""
-    });
+export type ScriptMetaProps = {
+    name: string;
+    namespace: string;
+    matchPattern: string;
+    ext: string;
+    textlintrc: string;
+};
+const useForm = (props: ScriptMetaProps) => {
+    const [name, setName] = useState(props.name);
+    const [namespace, setNamespace] = useState(props.namespace);
+    const [matchPattern, setMatchPattern] = useState(props.matchPattern);
+    const [ext, setExt] = useState(props.ext);
+    const [textlintrc, setTextlintrc] = useState(props.textlintrc);
+    const [errors, setErrors] = useState<
+        Partial<Record<"name" | "namespace" | "matchPattern" | "ext" | "textlintrc", Error | null>>
+    >({});
+    const isValid = useMemo(() => {
+        return Object.values(errors).filter((error) => error instanceof Error).length === 0;
+    }, [errors]);
+    const errorMessage = useMemo(() => {
+        return (
+            <div>
+                {Object.entries(errors)
+                    .filter(([_, error]) => error instanceof Error)
+                    .map(([key, error]) => {
+                        return (
+                            <p key={key}>
+                                <span>{key}</span>: {error?.message}
+                            </p>
+                        );
+                    })}
+            </div>
+        );
+    }, [errors]);
     const port = usePort();
-    const onSave = (textlintrc: string) => {
-        if (!props.name || !props.namespace) {
-            logger.error("props is wrong", props);
-            return;
-        }
-        port.updateScript({
-            name: props.name,
-            namespace: props.namespace,
-            textlintrc: textlintrc
-        });
-    };
     useEffect(() => {
         (async function loadScript() {
             if (!props.name || !props.namespace) {
@@ -41,20 +55,131 @@ export const EditPage = (props: EditPageProps) => {
             if (!script) {
                 return logger.error("script is not found", props);
             }
-            setMeta({
-                name: script.name,
-                namespace: script.namespace,
-                pattern: script.matchPattern,
-                ext: script.ext
-            });
-            setScript(script.textlintrc);
+            setName(script.name);
+            setNamespace(script.namespace);
+            setMatchPattern(script.matchPattern);
+            setExt(script.ext);
+            setTextlintrc(script.textlintrc);
         })();
     }, []);
+    const handlers = React.useMemo(
+        () => ({
+            name: (value: string) => {
+                setNamespace(value);
+                setErrors((prevState) => {
+                    return {
+                        ...prevState,
+                        name: value.length === 0 ? new Error("name length should be > 0") : null
+                    };
+                });
+            },
+            namespace: (value: string) => {
+                setNamespace(value);
+                setErrors((prevState) => {
+                    return {
+                        ...prevState,
+                        namespace: value.length === 0 ? new Error("namespace length should be > 0") : null
+                    };
+                });
+            },
+            matchPattern: (value: string) => {
+                setMatchPattern(value);
+                setErrors((prevState) => {
+                    return {
+                        ...prevState,
+                        matchPattern: value.length === 0 ? new Error("matchPattern length should be > 0") : null
+                    };
+                });
+            },
+            ext: (value: string) => {
+                setExt(value);
+                setErrors((prevState) => {
+                    if (value.length === 0) {
+                        return {
+                            ...prevState,
+                            ext: new Error("ext length should be > 0")
+                        };
+                    }
+                    if (!value.startsWith(".")) {
+                        return {
+                            ...prevState,
+                            ext: new Error("ext should starts with '.'")
+                        };
+                    }
+                    return {
+                        ...prevState,
+                        ext: null
+                    };
+                });
+            },
+            textlintrc: (value: string) => {
+                setTextlintrc(value);
+                setErrors((prevState) => {
+                    try {
+                        JSON.parse(value);
+                        return {
+                            ...prevState,
+                            textlintrc: value.length === 0 ? new Error("textlintrc length should be > 0") : null
+                        };
+                    } catch (error) {
+                        return {
+                            ...prevState,
+                            textlintrc: error
+                        };
+                    }
+                });
+            }
+        }),
+        []
+    );
+    return {
+        name,
+        namespace,
+        matchPattern,
+        ext,
+        textlintrc,
+        errors,
+        errorMessage,
+        isValid,
+        handlers
+    };
+};
+export const EditPage = (props: EditPageProps) => {
+    const port = usePort();
+    const { name, namespace, matchPattern, ext, textlintrc, handlers, errorMessage, isValid } = useForm({
+        name: props.name ?? "",
+        namespace: props.namespace ?? "",
+        matchPattern: "",
+        ext: "",
+        textlintrc: ""
+    });
+    const onSave = useCallback(() => {
+        if (!isValid) {
+            return;
+        }
+        port.updateScript({
+            name,
+            namespace,
+            matchPattern,
+            ext,
+            textlintrc
+        });
+    }, [name, namespace, matchPattern, ext, textlintrc]);
     return (
         <div className={"EditPage"}>
             <Flex direction="column" gap="size-100">
-                <ScriptMeta {...meta} />
-                <TextlintrcEditor textlintrc={script} onSave={onSave} />
+                <h3 id="label-3">Script Information</h3>
+                <Form maxWidth="size-3600" aria-labelledby="label-3">
+                    <TextField label="Namespace" value={namespace} isReadOnly={true} />
+                    <TextField label="Name" value={name} isReadOnly={true} />
+                    <TextField label="Pattern" value={matchPattern} minLength={1} onChange={handlers.matchPattern} />
+                    <TextField label="File Extension" value={ext} minLength={1} onChange={handlers.ext} />
+                </Form>
+                <TextlintrcEditor textlintrc={textlintrc} onChange={handlers.textlintrc} />
+                <ActionButton onPress={onSave} isDisabled={!isValid}>
+                    Save
+                </ActionButton>
+                {errorMessage}
             </Flex>
         </div>
     );
