9bddf54fa5dba736ff41e47f1d36f8f7707aa18b
azu <azuciao@gmail.com>
2021-05-02 17:15:04 +0900

    fix: sync version

---
+++ packages/webextension/tools/sync-version.js
@@ -0,0 +1,9 @@
+const fs = require("fs/promises");
+const path = require("path");
+const lerna = require("../../../lerna.json");
+(async function main() {
+    const manifestFilePath = path.resolve(__dirname, "../app/manifest.json");
+    const manifest = JSON.parse(await fs.readFile(manifestFilePath, "utf-8"));
+    manifest["version"] = lerna["version"];
+    await fs.writeFile(manifestFilePath, JSON.stringify(manifest, null, 4), "utf-8");
+})();
