0a710fa17994e641c03a49f90eea412c3c640da0
azu <azu@users.noreply.github.com>
2021-05-02 17:23:06 +0900

    fix(webextension): use tabs instead of webRequest (#45)
    * fix(webextension): use tabs instead of webRequest
    
    * chore: remove log
    
    * fix: sync version
    
    * chore: add coment

---
+++ packages/webextension/app/scripts/background/onTextlintWorker.ts
@@ -0,0 +1,33 @@
+import { browser } from "webextension-polyfill-ts";
+
+const isTextlintWorkerUrl = (urlString: string): boolean => {
+    try {
+        const url = new URL(urlString);
+        return /textlint-worker\.js$/.test(url.pathname);
+    } catch {
+        return false;
+    }
+};
+export type listenOnTextlintWorkerJsUrlArgs = {
+    onTextlintWorkerUrl: ({ tabId, url }: { tabId: number; url: string }) => void;
+};
+// On access http?://**/*textlint-worker.js
+export const listenOnTextlintWorkerJsUrl = (args: listenOnTextlintWorkerJsUrlArgs) => {
+    browser.tabs.onUpdated.addListener((tabId, changeInfo, _tab) => {
+        if (changeInfo && changeInfo.url && isTextlintWorkerUrl(changeInfo.url)) {
+            args.onTextlintWorkerUrl({
+                tabId,
+                url: changeInfo.url
+            });
+        }
+    });
+    browser.tabs.onActivated.addListener(async (activeInfo) => {
+        const tab = await browser.tabs.get(activeInfo.tabId);
+        if (tab && tab.id && tab.url && isTextlintWorkerUrl(tab.url)) {
+            args.onTextlintWorkerUrl({
+                tabId: tab.id,
+                url: tab.url
+            });
+        }
+    });
+};
