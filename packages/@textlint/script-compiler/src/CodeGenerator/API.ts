30b623997cbc187acf86e05f3dc089be25ff09fc
azu <azuciao@gmail.com>
2024-01-02 21:37:12 +0900

    fix typo

--- packages/@textlint/script-compiler/src/CodeGenerator/API.ts
+++ packages/@textlint/script-compiler/src/CodeGenerator/API.ts
@@ -7,7 +7,7 @@ export type API = {
     lint({ text, ext }: { text: string; ext: string }): TextlintResult[];
     // fix all text with all rule
     fixAll({ text, ext }: { text: string; ext: string }): TextlintFixResult;
-    // fix all with with a rule
+    // fix all with a rule
     fixRule({ text, ext, message }: { text: string; ext: string; message: TextlintMessage }): TextlintFixResult;
     // fix the text
     fixText({ text, ext, message }: { text: string; ext: string; message: TextlintMessage }): { output: string };
